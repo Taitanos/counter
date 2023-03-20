4ab3d147757583e0bc3a61e657b078d5e0a8d589
Taitanos <hayaashi87@gmail.com>
2023-03-20 22:29:53 +0300

    created simple counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
+    <div className={"app"}>
+      <div className={"input"}>
+          <input value={count}/>
+      </div>
+      <div className={"button"}>
+          <button onClick={() => {setCount(count + 1)}}>Add</button>
+          <button onClick={() => {setCount(0)}}>Reset</button>
+      </div>
     </div>
   );
 }
