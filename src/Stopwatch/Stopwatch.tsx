16efbeef1e037e326b0477d5cf4f1642432df1c6
Taitanos <hayaashi87@gmail.com>
2023-04-05 16:22:47 +0300

    test failed

--- src/Stopwatch/Stopwatch.tsx
+++ src/Stopwatch/Stopwatch.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Stopwatch.module.css'
 
 function Stopwatch() {
 
+    const [count, setCount] = useState(0);
+
+    const onClickStartHandler = () => {
+/*        let second : secondType = setInterval(() => (count + 1), 1000)
+        setCount(second);*/
+    };
+    const onClickStopHandler = () => {
+        setCount(count);
+    };
+    const onClickResetHandler = () => {
+        setCount(0);
+    };
+
     return (
         <div className={s.stopwatch}>
-            <input value={1}/>
+            <input value={count}/>
             <div>
-                <button>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                <button onClick={onClickStartHandler}>Start</button>
+                <button onClick={onClickStopHandler}>Stop</button>
+                <button onClick={onClickResetHandler}>Reset</button>
             </div>
         </div>
     );
