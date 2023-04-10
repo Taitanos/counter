0eeecc1c330ccfd332dedeca1788561340ff3d22
Taitanos <hayaashi87@gmail.com>
2023-04-10 14:29:55 +0300

    added scoreboard

--- src/ClickBox/ClickBox.tsx
+++ src/ClickBox/ClickBox.tsx
@@ -1,20 +1,33 @@
 import React, {useState} from 'react';
 import s from './ClickBox.module.css'
-import Counter from "../Counter/Counter";
 
 function ClickBox() {
 
     const [count, setCount] = useState(0);
 
     let add = () => {
-        setCount(count+1)
+        setCount(count + 1)
     }
 
+    let reset = () => {
+        setCount(0)
+    }
+
+
     return <div className={s.clickBox}>
-        <div className={s.input}>
-            <Counter/>
+        <div>
+            <div className={s.counter}>
+                <input className={s.input} value={count}/>
+                <div className={s.buttons}>
+                    <button className={s.button}>Record</button>
+                    <button className={s.button} onClick={reset}>Reset</button>
+                </div>
+            </div>
+            <div className={s.scoreboard}>
+
+            </div>
         </div>
-        <div className={s.field} onClick={add}></div>
+        <div className={s.field} onClick={add}>Click on me</div>
     </div>
 }
 
