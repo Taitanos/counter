7c5f650b29a6b0bc26863d1b2cac10de133808db
Taitanos <hayaashi87@gmail.com>
2023-05-24 20:56:44 +0300

    added navlink and path

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Counter from "./Contents/Counter/Counter";
+import Stopwatch from "./Contents/Stopwatch/Stopwatch";
+import ClickBox from "./Contents/ClickBox/ClickBox";
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                 <div className={"app-wrapper-content"}>
                     <Routes>
                         <Route path={'/counter'} element={<Counter/>}/>
+                        <Route path={'/stopwatch'} element={<Stopwatch/>}/>
+                        <Route path={'/clickbox'} element={<ClickBox/>}/>
                     </Routes>
                 </div>
             </div>
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -8,6 +8,12 @@ function Navbar() {
             <div className={s.item}>
                 <NavLink to={'/counter'} className={s.activeLink}>Counter</NavLink>
             </div>
+            <div className={s.item}>
+                <NavLink to={'/stopwatch'} className={s.activeLink}>Stopwatch</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/clickbox'} className={s.activeLink}>ClickBox</NavLink>
+            </div>
         </div>
     </div>
 }
