cdfbf91eb52029ecebfb4bd388654d690c8b8a8d
Taitanos <hayaashi87@gmail.com>
2023-05-24 14:13:28 +0300

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Counter from "./Contents/Counter/Counter";
 
 function App() {
     return (
-        <div className={"app-wrapper"}>
-            <Header/>
-            <Navbar/>
-            <div className={"app-wrapper-content"}>
-
+        <BrowserRouter>
+            <div className={"app-wrapper"}>
+                <Header/>
+                <Navbar/>
+                <div className={"app-wrapper-content"}>
+                    <Routes>
+                        <Route path={'/counter'} element={<Counter/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import s from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return <div className={s.nav}>
         <div className={s.navContent}>
-
+            <div className={s.item}>
+                <NavLink to={'/counter'} className={s.activeLink}>Counter</NavLink>
+            </div>
         </div>
     </div>
 }
